c3dd539c2a78f795ba15dbced3492c031554ee76
Postremus <panzer1996@googlemail.com>
2014-03-30 21:32:00 +0200

    zu git hinzugefügt

---
+++ src/com/Postremus/sawe/RegionListener.java
@@ -0,0 +1,144 @@
+package com.Postremus.sawe;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.masks.RegionMask;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
+public class RegionListener implements Listener {
+	private SaWe plugin;
+	
+	public RegionListener(SaWe plugin)
+	{
+		this.plugin = plugin;
+		this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+	}
+	
+	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled=true)
+	private void playerMoveHandler(PlayerMoveEvent event)
+	{
+		if (event.getTo().getBlockX() == event.getFrom().getBlockX() &&
+				event.getTo().getBlockY() == event.getFrom().getBlockY() &&
+				event.getTo().getBlockZ() == event.getFrom().getBlockZ())
+		{
+			return;
+		}
+		
+		Player p = event.getPlayer();
+		changeMask(p, event.getTo());
+	}
+	
+	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled=true)
+	public void playerJoinHandler(PlayerJoinEvent event)
+	{
+		Player p = event.getPlayer();
+		changeMask(p, p.getLocation());
+	}
+	
+	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled=true)
+	public void playerTeleportHandler(PlayerTeleportEvent event)
+	{
+		Player p = event.getPlayer();
+		changeMask(p, event.getTo());
+	}
+	
+	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled=true)
+	public void playerRespawnHandler(PlayerRespawnEvent event)
+	{
+		Player p = event.getPlayer();
+		changeMask(p, event.getRespawnLocation());
+	}
+	
+	
+	@EventHandler (priority = EventPriority.LOWEST, ignoreCancelled=true)
+	public void playerCommandHandler(PlayerCommandPreprocessEvent event)
+	{
+		WorldEditPlugin we = this.plugin.getWorldEdit();
+		String command = event.getMessage().split(" ")[0];
+		if (!isWorldEditCommand(command))
+		{
+			return;
+		}
+		Player p = event.getPlayer();
+		if (p.hasPermission("sawe.bypass"))
+		{
+			return;
+		}
+		if (we.getSession(p).getMask() == null)
+		{
+			this.plugin.sendMessage(p, "Du darfst hier kein Worldedit benutzen.");
+			event.setCancelled(true);
+		}
+	}
+	
+	private void changeMask(Player player, Location to)
+	{
+		WorldEditPlugin we = this.plugin.getWorldEdit();
+		if (player.hasPermission("sawe.bypass") || !player.hasPermission("sawe.use"))
+		{
+			we.getSession(player).setMask(null);
+			return;
+		}
+		ProtectedRegion own = getOwnRegionAt(player.getName(), to);
+		if (own == null)
+		{
+			return;
+		}
+		RegionMask rm = this.getRegionMask(own, to.getWorld());	
+		we.getSession(player).setMask(rm);
+	}
+	
+	private boolean isWorldEditCommand(String command)
+	{
+		if (command.startsWith("/"))
+		{
+			command = command.replaceFirst("/", "");
+		}
+		command = command.toLowerCase();
+		Map<String, String> weCommands = this.plugin.getWorldEdit().getWorldEdit().getCommandsManager().getCommands();
+		return weCommands.containsKey(command);
+	}
+	
+	private ProtectedRegion getOwnRegionAt(String playerName, Location loc)
+	{
+		RegionManager rm = this.plugin.getWorldGuard().getRegionManager(loc.getWorld());
+		ApplicableRegionSet set = rm.getApplicableRegions(loc);
+		for (ProtectedRegion rg : set)
+		{
+			if (rg.getOwners().contains(playerName))
+			{
+				return rg;
+			}
+		}
+		return null;
+	}
+	
+	private RegionMask getRegionMask(ProtectedRegion rg, World w)
+	{
+		CuboidRegion cr = new CuboidRegion(rg.getMinimumPoint(), rg.getMaximumPoint());
+		return new RegionMask(cr);
+	}
+}
---
+++ src/com/Postremus/sawe/SaWe.java
@@ -0,0 +1,40 @@
+package com.Postremus.sawe;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
+public class SaWe extends JavaPlugin{
+	RegionListener regioListener;
+	
+	@Override
+	public void onEnable() {
+		regioListener = new RegionListener(this);
+		this.getLogger().info("sagt Hallo");
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		HandlerList.unregisterAll(regioListener);
+		this.getLogger().info("sagt Bis Bald");
+	}
+	
+	public WorldGuardPlugin getWorldGuard() {
+	    return (WorldGuardPlugin) this.getServer().getPluginManager().getPlugin("WorldGuard");
+	}
+	
+	public WorldEditPlugin getWorldEdit()
+	{
+	    return (WorldEditPlugin) this.getServer().getPluginManager().getPlugin("WorldEdit");
+	}
+	
+	public void sendMessage(Player player, String message)
+	{
+		player.sendMessage("§a[SaWe]§7"+message);
+	}
+}
