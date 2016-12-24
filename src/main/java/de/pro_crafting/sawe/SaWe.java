2023d96adb84cf3d9b63c02c0a1571f47c68ea7f
Martin Panzer <postremus1996@googlemail.com>
2016-12-24 20:15:26 +0100

    cache instance of worldguard and worldedit Plugin lookups by name are costly. we are doing these every time a player moves so lets just save the plugin instances

--- src/main/java/de/pro_crafting/sawe/SaWe.java
+++ src/main/java/de/pro_crafting/sawe/SaWe.java
@@ -1,35 +1,38 @@
 package de.pro_crafting.sawe;
 
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-
 public class SaWe extends JavaPlugin{
-	RegionListener regioListener;
-	
-	
-	public void onEnable() {
-		regioListener = new RegionListener(this);
-		this.getLogger().info("sagt Hallo");		
-	}
-	
-	
-	public void onDisable() {
+    private RegionListener regioListener;
+    private WorldGuardPlugin worldGuardPlugin;
+    private WorldEditPlugin worldEditPlugin;
+
+    public void onEnable() {
+        regioListener = new RegionListener(this);
+        this.getLogger().info("says Hello");
+
+        worldGuardPlugin = JavaPlugin.getPlugin(WorldGuardPlugin.class);
+        worldEditPlugin = JavaPlugin.getPlugin(WorldEditPlugin.class);
+    }
+
+    public void onDisable() {
 		HandlerList.unregisterAll(regioListener);
-		this.getLogger().info("sagt Bis Bald");
-	}
-	
-	public WorldGuardPlugin getWorldGuard() {
-	    return (WorldGuardPlugin) this.getServer().getPluginManager().getPlugin("WorldGuard");
-	}
-	
-	public WorldEditPlugin getWorldEdit() {
-	    return (WorldEditPlugin) this.getServer().getPluginManager().getPlugin("WorldEdit");
-	}
-	
+        this.getLogger().info("says Bye Bye");
+    }
+
+    public WorldGuardPlugin getWorldGuard() {
+        return this.worldGuardPlugin;
+    }
+
+    public WorldEditPlugin getWorldEdit() {
+        return this.worldEditPlugin;
+    }
+
     public void sendMessage(Player player, String message) {
 		player.sendMessage("§a[SaWe]§7"+message);
 	}
