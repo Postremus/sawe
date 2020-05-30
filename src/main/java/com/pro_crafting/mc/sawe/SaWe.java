f13ff0e12b67865bb4d36eee1fb9f66f38b4a47d
Martin Panzer <postremus1996@googlemail.com>
2020-05-30 13:10:22 +0200

    Update to 1.14/1.15 (#1)

--- src/main/java/com/pro_crafting/mc/sawe/SaWe.java
+++ src/main/java/com/pro_crafting/mc/sawe/SaWe.java
@@ -1,6 +1,7 @@
-package de.pro_crafting.sawe;
+package com.pro_crafting.mc.sawe;
 
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 import org.bukkit.entity.Player;
@@ -8,15 +9,15 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SaWe extends JavaPlugin{
+    private final WorldGuardPlugin worldGuardPlugin = WorldGuardPlugin.inst();
+    private final WorldGuard worldGuard = com.sk89q.worldguard.WorldGuard.getInstance();
     private RegionListener regioListener;
-    private WorldGuardPlugin worldGuardPlugin;
     private WorldEditPlugin worldEditPlugin;
 
     public void onEnable() {
         regioListener = new RegionListener(this);
         this.getLogger().info("says Hello");
 
-        worldGuardPlugin = JavaPlugin.getPlugin(WorldGuardPlugin.class);
         worldEditPlugin = JavaPlugin.getPlugin(WorldEditPlugin.class);
     }
 
@@ -25,8 +26,12 @@ public class SaWe extends JavaPlugin{
         this.getLogger().info("says Bye Bye");
     }
 
-    public WorldGuardPlugin getWorldGuard() {
+    public WorldGuardPlugin getWorldGuardPlugin() {
         return this.worldGuardPlugin;
+    }
+
+    public WorldGuard getWorldGuard() {
+        return this.worldGuard;
     }
 
     public WorldEditPlugin getWorldEdit() {
